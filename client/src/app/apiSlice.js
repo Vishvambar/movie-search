b306d41d039fa1f2c81516bdeda1dcf60dd2c0d3
Vishvambar <161111635+Vishvambar@users.noreply.github.com>
2025-08-29 15:39:46 +0530

    commit

--- client/src/app/apiSlice.js
+++ client/src/app/apiSlice.js
@@ -1,8 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+const baseUrl = '/api';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }), // e.g. http://localhost:5000/api
+  baseQuery: fetchBaseQuery({ baseUrl}), // e.g. http://localhost:5000/api
   endpoints: (builder) => ({
     searchMovies: builder.query({
       query: ({ q, page = 1 }) =>
