81880411889adcc84b9501ddcfca7ffd9fb9b8d6
Vishvambar <161111635+Vishvambar@users.noreply.github.com>
2025-08-29 16:02:09 +0530

    commit

--- server/api/movies/[id].js
+++ server/api/movies/[id].js
@@ -1,4 +1,4 @@
-import { getmovies } from "../../src/services/omdb.js";
+import { getmovies } from "../../services/omdb.js";
 
 export default async function handler(req, res) {
   if (req.method !== "GET") {
---
+++ server/services/omdb.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const OMDB = axios.create({
+    baseURL: 'https://www.omdbapi.com/',
+    params: { apikey: process.env.OMDB_API_KEY }
+});
+
+export const searchmovies = async ({ query, page = 1 }) => {
+    const data = await OMDB.get('/', {
+        params: { s: query, page },
+    });
+    return data;
+};
+
+export const getmovies = async (id) => {
+    const data = await OMDB.get('/', {
+        params: { i: id, plot: "full" },
+    });
+    return data;
+};
